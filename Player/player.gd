9a59f41f092c4f8b0b18dcfd3163bbb8a58a4eb3
Samar <samarjomysujith606@gmail.com>
2025-09-29 20:47:58 +0530

    Added extra camera movement and assets

--- Player/player.gd
+++ Player/player.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = 4.5
 
 var mouse_motion=Vector2.ZERO
 
+@onready var camerapivot:Node3D=$CameraPivot
+
 func _ready() -> void:
 	Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
 
@@ -42,5 +44,7 @@ func _input(event: InputEvent) -> void:
 		
 func handle_camera_rotation() -> void:
 	rotate_y(mouse_motion.x)
+	camerapivot.rotate_x(mouse_motion.y)
+	camerapivot.rotation_degrees.x=clampf(camerapivot.rotation_degrees.x,-90.0,90.0)
 	mouse_motion=Vector2.ZERO
 	
