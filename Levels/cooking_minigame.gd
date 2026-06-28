ea4cdc72a9ddeb25be569e7e04dc5b038cf4baf4
Samar <samarjomysujith606@gmail.com>
2026-06-28 10:48:21 +0530

    alternate implementation

--- Levels/cooking_minigame.gd
+++ Levels/cooking_minigame.gd
@@ -5,22 +5,31 @@ extends Node2D
 
 var food_entered:bool=false
 var lmb_event:bool=true
-var run_once:bool=false
+var run_once:bool=false #can be set to false again once the food (the body) exits the area again, use signal
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index==MOUSE_BUTTON_LEFT and !event.pressed and food_entered==true and run_once==false:
+			run_once=true
+			food.visible=false
+			animation.visible=true
+			animation.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if food_entered==true:
+	pass
+'	if food_entered==true:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)==false:
-			food.visible=false	
+			food.visible=false
 			if run_once==false:
 				animation.visible=true
 				animation.play()
-				run_once=true
+				run_once=true'
 
 
 func _on_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
